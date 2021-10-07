dd4602c59a6d6874fb3d9bcb51389401307b284d
Zilvinas-Zitkauskas <92127488+Zilvinas-Zitkauskas@users.noreply.github.com>
2021-10-08 02:12:42 +0300

    Delete delete.js

--- scripts/delete.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(document).on('mouseenter', '.divbutton', function() {
-        $(this).find(":button").show();
-    }).on('mouseleave', '.divbutton', function() {
-        $(this).find(":button").hide();
-    }).on('click', ':button', function() {
-        $(this).parent().remove();
-    });
-});
